df0d096ad6046b195f9e2c2a5187623ece1a40e4
Daniel <64807656+DanielNinchev@users.noreply.github.com>
2020-11-11 18:10:55 +0200

    initial uppload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ShellSort
+{
+    class Program
+    {
+        //сложен алгоритъм за сортиране
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 46, 60, 56, 81, 16 };
+
+            for (int gap = array.Length / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < array.Length; i++)
+                {
+                    int value = array[i];
+                    int j;
+
+                    for (j = i; j >= gap && array[j-gap] > value; j -= gap)
+                    {
+                        array[j] = array[j - gap];
+                    }
+
+                    array[j] = value;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
